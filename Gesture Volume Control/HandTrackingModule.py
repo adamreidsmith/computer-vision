561bb61219a591dd488908b6f03ae52cadce7c7e
Adam Smith <62571017+adamreidsmith@users.noreply.github.com>
2022-04-05 19:08:58 -0700

    Update HandTrackingModule.py

--- Gesture Volume Control/HandTrackingModule.py
+++ Gesture Volume Control/HandTrackingModule.py
@@ -49,7 +49,6 @@ class handDetector:
 def main():
     cap = cv2.VideoCapture(0)
 
-    max_fps = 30
     t_prev = t_now = 0
 
     detector = handDetector()
@@ -58,13 +57,9 @@ def main():
         success, img = cap.read()
         img = detector.find_hands(img)
         lmk_list = detector.find_position(img)
-        if len(lmk_list) != 0:
-            print(lmk_list[0])
-            
-        elapsed = time.time() - t_prev
-        if 1/elapsed > max_fps:
-            time.sleep(1/max_fps - elapsed)
-
+#         if len(lmk_list) != 0:
+#             print(lmk_list[0])
+        
         t_now = time.time()
         fps = 1/(t_now - t_prev)
         t_prev = t_now
